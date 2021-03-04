d5070eadb98beec642091ada3e28cfe8717884c9
wingedthing <jcarlo@gmail.com>
2021-03-04 16:52:39 -0500

    Create msgModule api from simpleMsg.js for later use by polling script.

---
+++ codes/msgModule.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * @param {*} urlString String url of the webhook to send post to. Required
+ * @param {*} content String content of the post msg. Required
+ * @param {*} username Display name of the discord bot that sent the msg to channel. Optional
+ * @param {*} avatar_url url link to display img of discord bot. Optional
+ */
+const msgModule = (urlString, content, username, avatar_url) => {
+  //npm install node-fetch
+  const fetch = require('node-fetch');
+  //npm install fs
+  const fs = require('fs');
+  let _whurl = "";
+  const _msg = {};
+  
+  const sendMsg = () => {
+    _whurl = urlString;
+    _msg.content = content;
+    if(username) _msg.username = username;
+    if(avatar_url) _msg.avatar_url = avatar_url;
+    
+    //Creates a promise, POSTS msg and waits for the result from discord server. Appends the result to msg.log  
+    fetch(_whurl + "?wait=true", {"method":"POST", "headers":{"content-type": "application/json"},"body": JSON.stringify(_msg)})
+      .then(res => res.json())
+      .then(json => {
+        const stream = fs.createWriteStream('./msg.log', {flags: 'a'});
+        for(let key in json) {
+          stream.write(`${key}: ${JSON.stringify(json[key], null, 2)}\n`);
+        }
+        stream.write('\n\n');
+        });
+      
+  }
+
+  sendMsg();
+
+};
+
+exports.send = msgModule;
